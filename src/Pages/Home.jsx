a4d25053090b1f63951210656af71f48cb83a59c
fred0727 <freddymh07952023@gmail.com>
2023-10-06 07:34:34 -0500

    Updating page projects

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -262,8 +262,8 @@ const Home = () => {
               Proyectos Recientes
             </h2>
             <section className="w-full grid grid-cols-[repeat(auto-fill,_250px)] xxm:grid-cols-[repeat(auto-fill,_280px)] xxs:grid-cols-[repeat(auto-fill,325px)] xmd:grid-cols-[repeat(auto-fill,600px)] xlg:grid-cols-[repeat(auto-fill,425px)] xxl:grid-cols-[repeat(auto-fill,550px)] pt-6 gap-8 justify-center mx-auto xlg:gap-12">
-              <CardProject project="weatherapp" />
               <CardProject project="crudcodeigniter" />
+              <CardProject project="weatherapp" />
               <CardProject project="ecomercereact" />
               <CardProject project="academlostore" />
             </section>
